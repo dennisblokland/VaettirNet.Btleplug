abecc442c42a630a54580bb07f65dbc6e6ecfcd3
Chad Nedzlek <chadnedz@microsoft.com>
2024-02-27 17:16:33 -0800

    Include linux setup

--- setup.bat
+++ setup.bat
@@ -5,8 +5,8 @@ mkdir %DLD%
 curl -L https://github.com/ChadNedzlek/btleplug-c/releases/download/%VERSION%/binaries.zip --fail -o %DLD%\binaries.zip
 mkdir %DLD%\extracted
 tar -xf %DLD%\binaries.zip --directory %DLD%\extracted
-mkdir %RUNTIMES%\win-x64\
+mkdir -p %RUNTIMES%\win-x64\
 copy /Y %DLD%\extracted\x86_64-pc-windows-gnu\release\* %RUNTIMES%\win-x64\
-mkdir %RUNTIMES%\linux-x64\
+mkdir -p %RUNTIMES%\linux-x64\
 copy /Y %DLD%\extracted\x86_64-unknown-linux-gnu\release\* %RUNTIMES%\linux-x64\
 rmdir /S /Q %DLD%
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DLD=$(mktemp -d)
+RUNTIMES="$(dirname "$(readlink -f "$0")")/VaettirNet.Btleplug/runtimes"
+VERSION="0.0.2"
+
+curl -L "https://github.com/ChadNedzlek/btleplug-c/releases/download/$VERSION/binaries.zip" --fail -o $DLD/binaries.zip
+mkdir -p $DLD/extracted
+unzip -q $DLD/binaries.zip -d $DLD/extracted
+mkdir -p $RUNTIMES/win-x64/
+cp -r -f -t $RUNTIMES/win-x64/ $DLD/extracted/x86_64-pc-windows-gnu/release/* 
+mkdir -p $RUNTIMES/linux-x64/
+cp -r -f -t $RUNTIMES/linux-x64/ $DLD/extracted/x86_64-unknown-linux-gnu/release/* 
+#rm -r "$DLD"
