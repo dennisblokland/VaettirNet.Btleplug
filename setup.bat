a0c689c9e2ebe78eec153d373bf4919217339e98
Chad Nedzlek <chadnedz@microsoft.com>
2024-02-29 21:30:33 -0800

    Update to version 0.0.7 of btleplug-c
    This update should enable osx/macos usage of the package

--- setup.bat
+++ setup.bat
@@ -1,12 +1,16 @@
 SET DLD=%TEMP%\%RANDOM%%RANDOM%
 SET RUNTIMES=%~dp0VaettirNet.Btleplug\runtimes
-SET VERSION=0.0.5
+SET /P VERSION=<%~dp0\btleplug-c.version.txt
 mkdir %DLD%
-curl -L https://github.com/ChadNedzlek/btleplug-c/releases/download/%VERSION%/binaries.zip --fail -o %DLD%\binaries.zip
+curl -L https://github.com/ChadNedzlek/btleplug-c/releases/download/%VERSION%/binaries.tar.gz --fail -o %DLD%\binaries.tar.gz
 mkdir %DLD%\extracted
-tar -xf %DLD%\binaries.zip --directory %DLD%\extracted
+tar -xf %DLD%\binaries.tar.gz --directory %DLD%\extracted
 mkdir %RUNTIMES%\win-x64\ 2>nul
-copy /Y %DLD%\extracted\x86_64-pc-windows-gnu\release\* %RUNTIMES%\win-x64\
+copy /Y %DLD%\extracted\target\win-x64\x86_64-pc-windows-msvc\release\* %RUNTIMES%\win-x64\
 mkdir %RUNTIMES%\linux-x64\ 2>nul
-copy /Y %DLD%\extracted\x86_64-unknown-linux-gnu\release\* %RUNTIMES%\linux-x64\
+copy /Y %DLD%\extracted\target\linux-x64\x86_64-unknown-linux-gnu\release\* %RUNTIMES%\linux-x64\
+mkdir %RUNTIMES%\osx-arm64\ 2>nul
+copy /Y %DLD%\extracted\target\macos-arm64\aarch64-apple-darwin\release\* %RUNTIMES%\osx-arm64\
+mkdir %RUNTIMES%\osx-x64\ 2>nul
+copy /Y %DLD%\extracted\target\macos-x64\x86_64-apple-darwin\release\* %RUNTIMES%\osx-x64\
 rmdir /S /Q %DLD%
--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-DLD=$(mktemp -d)
-RUNTIMES="$(dirname "$(readlink -f "$0")")/VaettirNet.Btleplug/runtimes"
-VERSION="0.0.5"
+set -x
 
-curl -L "https://github.com/ChadNedzlek/btleplug-c/releases/download/$VERSION/binaries.zip" --fail -o $DLD/binaries.zip
+DLD=$(mktemp -d)
+RUNDIR="$(dirname "$(readlink -f "$0")")"
+RUNTIMES="$RUNDIR/VaettirNet.Btleplug/runtimes"
+VERSION=$(< $RUNDIR/btleplug-c.version.txt)
+
+curl -L "https://github.com/ChadNedzlek/btleplug-c/releases/download/$VERSION/binaries.tar.gz" --fail -o $DLD/binaries.tar.gz
 mkdir -p $DLD/extracted
-unzip -q $DLD/binaries.zip -d $DLD/extracted
+tar -xf $DLD/binaries.tar.gz --directory $DLD/extracted
 mkdir -p $RUNTIMES/win-x64/
-cp -r -f -t $RUNTIMES/win-x64/ $DLD/extracted/x86_64-pc-windows-gnu/release/* 
+cp -rf $DLD/extracted/target/win-x64/x86_64-pc-windows-msvc/release/* -t $RUNTIMES/win-x64/
 mkdir -p $RUNTIMES/linux-x64/
-cp -r -f -t $RUNTIMES/linux-x64/ $DLD/extracted/x86_64-unknown-linux-gnu/release/* 
-#rm -r "$DLD"
+cp -rf $DLD/extracted/target/linux-x64/x86_64-unknown-linux-gnu/release/* -t $RUNTIMES/linux-x64/
+mkdir -p $RUNTIMES/osx-arm64/
+cp -rf $DLD/extracted/target/macos-arm64/aarch64-apple-darwin/release/* -t $RUNTIMES/osx-arm64/
+mkdir -p $RUNTIMES/osx-x64/
+cp -rf $DLD/extracted/target/macos-x64/x86_64-apple-darwin/release/* -t $RUNTIMES/osx-x64/
+rm -r "$DLD"
