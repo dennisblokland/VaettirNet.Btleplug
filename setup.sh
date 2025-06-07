e9183d5f7d0d18bb0919a10451560f48a3648652
Dennis Blokland <dennishoornaar@live.nl>
2025-06-07 11:22:54 +0200

    publish to my nuget

--- setup.sh
+++ setup.sh
@@ -7,13 +7,15 @@ RUNDIR="$(dirname "$(readlink -f "$0")")"
 RUNTIMES="$RUNDIR/VaettirNet.Btleplug/runtimes"
 VERSION=$(< $RUNDIR/btleplug-c.version.txt)
 
-curl -L "https://github.com/ChadNedzlek/btleplug-c/releases/download/$VERSION/binaries.tar.gz" --fail -o $DLD/binaries.tar.gz
+curl -L "https://github.com/dennisblokland/btleplug-c/releases/download/$VERSION/binaries.tar.gz" --fail -o $DLD/binaries.tar.gz
 mkdir -p $DLD/extracted
 tar -xf $DLD/binaries.tar.gz --directory $DLD/extracted
 mkdir -p $RUNTIMES/win-x64/
 cp -rf $DLD/extracted/target/win-x64/x86_64-pc-windows-msvc/release/* -t $RUNTIMES/win-x64/
 mkdir -p $RUNTIMES/linux-x64/
 cp -rf $DLD/extracted/target/linux-x64/x86_64-unknown-linux-gnu/release/* -t $RUNTIMES/linux-x64/
+mkdir -p $RUNTIMES/linux-arm64/
+cp -rf $DLD/extracted/target/linux-arm64/aarch64-unknown-linux-gnu/release* -t $RUNTIMES/linux-arm64/
 mkdir -p $RUNTIMES/osx-arm64/
 cp -rf $DLD/extracted/target/macos-arm64/aarch64-apple-darwin/release/* -t $RUNTIMES/osx-arm64/
 mkdir -p $RUNTIMES/osx-x64/
